9adfe52026283a137dd92b28ca347d29dddcedac
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-09 22:11:13 +0600

    created navitems in a separate js file so that it can be looped for reducing complexity

---
+++ src/Utilities/navMenu.js
@@ -0,0 +1,52 @@
+export const navMenu = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    isVisible: true,
+  },
+  {
+    id: 2,
+    title: "Dashboard",
+    path: "/dashboard",
+    isVisible: true,
+  },
+  {
+    id: 3,
+    title: "About Us",
+    path: "/about-us",
+    isVisible: true,
+  },
+  {
+    id: 4,
+    title: "Contact Us",
+    path: "/contact-us",
+    isVisible: true,
+  },
+];
+export const sidebarMenu = [
+  {
+    id: 1,
+    title: "Overview",
+    path: "/dashboard/overview",
+    isVisible: true,
+  },
+  {
+    id: 2,
+    title: "All Fish",
+    path: "/dashboard/all-fish",
+    isVisible: true,
+  },
+  {
+    id: 3,
+    title: "Registered Customer",
+    path: "/dashboard/registered-customer",
+    isVisible: true,
+  },
+  {
+    id: 4,
+    title: "Orders",
+    path: "/dashboard/orders",
+    isVisible: true,
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        text: "rgb(51, 63, 77)",             // For main text, body content, paragraphs, headings
-        background: "rgb(230, 241, 247)",    // For page background, section backgrounds, cards
-        primary: "rgb(11, 60, 93)",          // For navbar, footer, primary buttons (like "Add to cart", "Login"), key elements
-        secondary: "rgb(127, 205, 255)",     // For hover states, secondary buttons, badges, tab highlights
-        accent: "rgb(227, 206, 102)",        // For highlight areas like discount tags, "New", "Popular", price emphasis
-      }
-      
+        text: "rgb(51, 63, 77)", // For main text, body content, paragraphs, headings
+        background: "rgb(230, 241, 247)", // For page background, section backgrounds, cards
+        primary: "rgb(11, 60, 93)", // For navbar, footer, primary buttons (like "Add to cart", "Login"), key elements
+        secondary: "rgb(127, 205, 255)", // For hover states, secondary buttons, badges, tab highlights
+        accent: "rgb(227, 206, 102)", // For highlight areas like discount tags, "New", "Popular", price emphasis
+      },
     },
   },
   plugins: [require("daisyui")],

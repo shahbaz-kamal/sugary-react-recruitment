0b9ca162a91aa5e464aebd694a57a112a6860050
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-10 01:03:43 +0600

    updated hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -15,13 +15,12 @@ const Hero = () => {
       <div className="hero-overlay bg-black bg-opacity-70"></div>
       <div className="text-neutral-content w-11/12 mx-auto flex flex-col-reverse md:flex-row justify-between items-center py-10 gap-10">
         {/* Left side */}
-        <div className="text-center md:text-left max-w-xl">
-          <h1 className="mb-5 text-5xl md:text-6xl lg:text-7xl font-bold text-primary font-cinzel">
+        <div className="text-center md:text-left ">
+          <h1 className="mb-5 text-5xl md:text-6xl lg:text-7xl font-bold text-secondary font-cinzel">
             Dive into Tranquility
           </h1>
           <p className="mb-5 text-2xl md:text-4xl">
-            Aquarium <br />
-            Fish Sale
+          Bring the Ocean to Your Living Room
           </p>
           <div className="flex gap-4 justify-center md:justify-start">
             <button className="rounded-md border border-secondary px-6 py-2 text-lg bg-secondary hover:bg-secondary hover:bg-opacity-80 hover:border-secondary hover:border-opacity-60 text-text transition duration-300 font-semibold">
@@ -34,7 +33,7 @@ const Hero = () => {
         </div>
 
         {/* Right side */}
-        <div className="w-full md:w-1/2 flex justify-center">
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
           <img
             src={sideImage}
             className="w-[320px] md:w-[270px] lg:w-[500px]  object-cover"

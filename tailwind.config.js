27c69d9c5aecccd6e40f26fb9a65fd8c5cbd3ce6
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-09 21:05:17 +0600

    color palletes selected

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "rgb(51, 63, 77)",             // For main text, body content, paragraphs, headings
+        background: "rgb(230, 241, 247)",    // For page background, section backgrounds, cards
+        primary: "rgb(11, 60, 93)",          // For navbar, footer, primary buttons (like "Add to cart", "Login"), key elements
+        secondary: "rgb(127, 205, 255)",     // For hover states, secondary buttons, badges, tab highlights
+        accent: "rgb(227, 206, 102)",        // For highlight areas like discount tags, "New", "Popular", price emphasis
+      }
+      
+    },
   },
-  plugins: [require('daisyui'),],
-}
+  plugins: [require("daisyui")],
+};

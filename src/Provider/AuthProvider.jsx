0b6fc28e63d2ce8fd6981556c9b6974d2837e61a
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-10 15:52:41 +0600

    login functionality working

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export const AuthContext = createContext(null);
 
@@ -9,16 +10,37 @@ const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   const login = async (userName, password) => {
+    setLoading(true)
     const res = await axios.post(
       `https://sugarytestapi.azurewebsites.net/AdminAccount/Login`,
       { UserName: userName, Password: password }
     );
     console.log(res);
+    // const data = await res.json();
+    if (res.data.Success) {
+      localStorage.setItem("accessToken", res.data.Token);
+      localStorage.setItem("refreshToken", res.data.RefreshToken);
+      setUser(res.data.User);
+      // navigate("/dashboard");
+      Swal.fire({
+        title: "Login Successful",
+        icon: "success",
+        draggable: true
+      });
+      setLoading(false)
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `${error.message}`,
+      });
+    }
+    console.log(res);
     // const data=await res.json()
     // console.log(data)
   };
 
-  const authInfo = { login };
+  const authInfo = { login,user,loading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
